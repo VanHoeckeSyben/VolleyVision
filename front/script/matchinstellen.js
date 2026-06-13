aeef0e9bd37d1c02df39ab6865ef074e5e3ea0b8
VanHoeckeSyben <syben.van.hoecke@student.howest.be>
2026-06-13 16:27:33 +0200

    feat: aanmaken match instellen pagina

---
+++ front/script/matchinstellen.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const lanIP = `${window.location.hostname}:8000`;
+const API = `http://127.0.0.1:8000/api/v1`
+const socketio = io(lanIP);
+
+// #region ***  DOM references                           ***********
+// #endregion
+
+// #region ***  Callback-Visualisation - show___         ***********
+// #endregion
+
+// #region ***  Callback-No Visualisation - callback___  ***********
+// #endregion
+
+// #region ***  Data Access - get___                     ***********
+// #endregion
+
+// #region ***  Event Listeners - listenTo___            ***********
+// #endregion
+
+// #region ***  Init / DOMContentLoaded                  ***********
+const init = () => {
+    
+}
+
+document.addEventListener('DOMContentLoaded',init);
+// #endregion
